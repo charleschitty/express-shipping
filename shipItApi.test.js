5ff1de874bb1da506f1f44beceaf5413e7762e62
Charles Chitty <charlescchitty@gmail.com>
2024-01-09 17:45:50 -0800

    Started Mock Fetch tests

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,18 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const fetchMock = require("fetch-mock");
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
+
+shipProduct = jest.fn();
 
 
 test("shipProduct", async function () {
+  fetchMock.post(`${SHIPIT_SHIP_URL}`, {
+    body: {
+      1610
+    },
+    status: 200
+  })
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +20,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(1610);
 });
