3b0b3acfc1bdf891c6bfa706a49199f793d893db
Charles Chitty <charlescchitty@gmail.com>
2024-01-09 17:33:30 -0800

    Fixed code review syntax and tests

--- routes/shipments.js
+++ routes/shipments.js
@@ -10,20 +10,26 @@ const { shipProduct } = require("../shipItApi");
 
 /** POST /ship
  *
- * VShips an order coming from json body:
+ * Validate and Ships an order coming from json body:
  *   { productId, name, addr, zip }
  *
  * Returns { shipped: shipId }
  */
 
 router.post("/", async function (req, res, next) {
-  //format validate on separate line
+
   const result = jsonschema.validate(
-    req.body, orderShipmentSchema, {required: true});
+    req.body,
+    orderShipmentSchema,
+    {
+      required: true
+    });
+
   if(!result.valid){
     const errs = result.errors.map(err => err.stack);
     throw new BadRequestError(errs);
   }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -40,100 +40,54 @@ describe("POST /", function () {
   //add expect for error message
   //don't need as many tests for testing json schema requirements
   //combine tests into one, all three missing, all three wrong types
-  test("throws error if no productId", async function () {
+  test ("throws error if missing all properties (Empty JSON)", async function() {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({}
+      );
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+			"instance requires property \"productId\"",
+			"instance requires property \"name\"",
+			"instance requires property \"addr\"",
+			"instance requires property \"zip\""
+		]);
+  });
+
+  test ("throws error if missing some properties", async function(){
     const resp = await request(app)
       .post("/shipments")
       .send(
         {
-          name: "Test Tester",
-          addr: "100 Test St",
-          zip: "12345-6789",
+          name: "Test Tester"
         }
       );
     expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+			"instance requires property \"productId\"",
+			"instance requires property \"addr\"",
+			"instance requires property \"zip\""
+		]);
   });
 
-  test("throws error if name is invalid", async function () {
+  test("throws error if multiple invalid properties", async function() {
     const resp = await request(app)
       .post("/shipments")
       .send(
         {
-          productId: "1000",
-          name: 543,
-          addr: "100 Test St",
-          zip: "12345-6789",
+          productId: "1AA",
+          name: 123,
+          addr: 1234,
+          zip: 12345
         }
       );
     expect(resp.statusCode).toEqual(400);
-  });
-
-  test("throws error if no name", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          productId: "1000",
-          addr: "100 Test St",
-          zip: "12345-6789",
-        }
-      );
-    expect(resp.statusCode).toEqual(400);
-    //add expect for error message
-  });
-
-  test("throws error if addr is invalid", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          productId: "1000",
-          name: "Test Tester",
-          addr: 8765,
-          zip: "12345-6789",
-        }
-      );
-    expect(resp.statusCode).toEqual(400);
-  });
-
-  test("throws error if no addr", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          productId: "1000",
-          name: "Test Tester",
-          zip: "12345-6789",
-        }
-      );
-    expect(resp.statusCode).toEqual(400);
-  });
-
-  test("throws error if invalid zip", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          productId: "1000",
-          name: "Test Tester",
-          addr: "100 Test St",
-          zip: "1a5-6789",
-        }
-      );
-    expect(resp.statusCode).toEqual(400);
-  });
-
-  test("throws error if no zip", async function () {
-    const resp = await request(app)
-      .post("/shipments")
-      .send(
-        {
-          productId: "1000",
-          name: "Test Tester",
-          addr: "100 Test St",
-        }
-      );
-    expect(resp.statusCode).toEqual(400);
-  });
-
+    expect(resp.body.error.message).toEqual([
+			"instance.productId is not of a type(s) integer",
+			"instance.name is not of a type(s) string",
+			"instance.addr is not of a type(s) string",
+			"instance.zip is not of a type(s) string"
+		]);
+  })
 
 });
