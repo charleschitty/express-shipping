f17925c9380fa25381ee9f93e62d1900bc8a3367
Eric Huie <erichuie97@gmail.com>
2024-01-09 17:07:06 -0800

    added some tests for schema validation

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -22,4 +22,118 @@ describe("POST /", function () {
       .send();
     expect(resp.statusCode).toEqual(400);
   });
+
+  test("throws error if productId is invalid", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: "Test Tester",
+          addr: "100 Test St",
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  //add expect for error message
+  //don't need as many tests for testing json schema requirements
+  //combine tests into one, all three missing, all three wrong types
+  test("throws error if no productId", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          name: "Test Tester",
+          addr: "100 Test St",
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if name is invalid", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: 543,
+          addr: "100 Test St",
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if no name", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          addr: "100 Test St",
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+    //add expect for error message
+  });
+
+  test("throws error if addr is invalid", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: "Test Tester",
+          addr: 8765,
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if no addr", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: "Test Tester",
+          zip: "12345-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if invalid zip", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: "Test Tester",
+          addr: "100 Test St",
+          zip: "1a5-6789",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("throws error if no zip", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send(
+        {
+          productId: "1000",
+          name: "Test Tester",
+          addr: "100 Test St",
+        }
+      );
+    expect(resp.statusCode).toEqual(400);
+  });
+
+
 });
